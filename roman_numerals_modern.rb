695b6f0011dd8e1ad9627d418e5d7f76fbb79036
stearagon <stearagon@gmail.com>
2015-04-02 21:45:00 -0400

    first commit

---
+++ roman_numerals_modern.rb
@@ -0,0 +1,24 @@
+def roman_numeral(x)
+  roman_numerals = [[1,"I"],[4,"IV"],[5,"V"],
+  [9,"IX"],[10,"X"],[40,"XL"],[50,"L"],[90,"XC"],
+  [100,"C"],[400,"CD"],[500,"D"],[900,"CM"],[1000,"M"]]
+
+  numerals = ""
+  i = 12
+  while x > 0
+    search = true
+    while search == true
+      y = roman_numerals[i][0]
+      if x >= y
+        search = false
+        (x/y).times do numerals << roman_numerals[i][1] end
+        x = x % y
+      elsif
+        i -= 1
+      end
+    end
+  end
+  numerals
+end
+
+puts roman_numeral(2439)
